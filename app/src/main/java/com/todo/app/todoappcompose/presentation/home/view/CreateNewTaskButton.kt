a660cd2a72ed2265fc9a45ce09b13cf540c6390a
Lyamin Tikhon <misterkukuruza784@gmail.com>
2024-06-22 04:20:16 +0300

    Creating Home Screen and its components, updating data from the repository.

---
+++ app/src/main/java/com/todo/app/todoappcompose/presentation/home/view/CreateNewTaskButton.kt
@@ -0,0 +1,41 @@
+package com.todo.app.todoappcompose.presentation.home.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.todo.app.todoappcompose.R
+import com.todo.app.todoappcompose.app.theme.AppTheme
+
+@Composable
+fun CreateNewTaskBtn(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier
+            .fillMaxWidth()
+            .background(AppTheme.colorScheme.backSecondary)
+            .clickable { onClick.invoke() }
+            .padding(
+                start = 16.dp,
+                end = 16.dp,
+                top = 12.dp,
+                bottom = 12.dp
+            ),
+    ) {
+        Text(
+            modifier = Modifier
+                .padding(start = 36.dp, top = 2.dp),
+            text = stringResource(id = R.string.new_title),
+            color = AppTheme.colorScheme.labelTertiary,
+            style = AppTheme.typographyScheme.body,
+        )
+    }
+}
