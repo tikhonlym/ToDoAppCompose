268f45f305e2a5b4b7b1f8036a88677e710c3abd
Lyamin Tikhon <misterkukuruza784@gmail.com>
2024-06-22 04:15:48 +0300

    Setting up resources

--- app/src/main/java/com/todo/app/todoappcompose/app/MainActivity.kt
+++ app/src/main/java/com/todo/app/todoappcompose/app/MainActivity.kt
@@ -7,28 +7,30 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
-import com.todo.app.todoappcompose.app.theme.ToDoAppComposeTheme
-import com.todo.app.todoappcompose.presentation.edit.EditToDoScreen
-import com.todo.app.todoappcompose.presentation.edit.EditToDoScreenDestination
+import com.todo.app.todoappcompose.app.theme.AppTheme
+import com.todo.app.todoappcompose.presentation.edit.EditScreen
+import com.todo.app.todoappcompose.presentation.edit.EditScreenDestination
 import com.todo.app.todoappcompose.presentation.home.HomeScreen
 import com.todo.app.todoappcompose.presentation.home.HomeScreenDestination
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ToDoAppComposeTheme {
+            AppTheme {
                 val navController = rememberNavController()
                 NavHost(
                     navController = navController,
                     startDestination = HomeScreenDestination
                 ) {
                     composable<HomeScreenDestination> {
-                        HomeScreen(navController)
+                        HomeScreen(navController = navController)
                     }
-                    composable<EditToDoScreenDestination> {
-                        val inputArguments = it.toRoute<EditToDoScreenDestination>()
-                        EditToDoScreen(
+                    composable<EditScreenDestination> {
+                        val inputArguments = it.toRoute<EditScreenDestination>()
+                        EditScreen(
                             id = inputArguments.id,
                             navController = navController
                         )
--- app/src/main/java/com/todo/app/todoappcompose/app/theme/Theme.kt
+++ app/src/main/java/com/todo/app/todoappcompose/app/theme/Theme.kt
@@ -1,22 +1,37 @@
 package com.todo.app.todoappcompose.app.theme
 
+import android.app.Activity
 import androidx.compose.foundation.LocalIndication
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 import com.todo.app.todoappcompose.app.theme.local.AppColorScheme
 import com.todo.app.todoappcompose.app.theme.local.AppTypography
 import com.todo.app.todoappcompose.app.theme.local.LocalAppColorScheme
 import com.todo.app.todoappcompose.app.theme.local.LocalAppTypography
 
 @Composable
-fun ToDoAppComposeTheme(
+fun AppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit,
 ) {
     val colorScheme = if (darkTheme) darkColorScheme else lightColorScheme
     val rippleIndication = rememberRipple()
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.backPrimary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+        }
+    }
+
     CompositionLocalProvider(
         LocalAppColorScheme provides colorScheme,
         LocalAppTypography provides typography,
@@ -25,10 +40,10 @@ fun ToDoAppComposeTheme(
     )
 }
 
-object ToDoAppComposeTheme {
+object AppTheme {
     val colorScheme: AppColorScheme
         @Composable get() = LocalAppColorScheme.current
-    val typography: AppTypography
+    val typographyScheme: AppTypography
         @Composable get() = LocalAppTypography.current
 }
 
@@ -42,6 +57,7 @@ private val darkColorScheme = AppColorScheme(
     colorRed = ColorRedDark,
     colorGreen = ColorGreenDark,
     colorBlue = ColorBlueDark,
+    colorLightBlue = ColorLightBlueDark,
     colorGray = ColorGrayDark,
     colorGrayLight = ColorGrayLightDark,
     colorWhite = ColorWhiteDark,
@@ -60,6 +76,7 @@ private val lightColorScheme = AppColorScheme(
     colorRed = ColorRed,
     colorGreen = ColorGreen,
     colorBlue = ColorBlue,
+    colorLightBlue = ColorLightBlue,
     colorGray = ColorGray,
     colorGrayLight = ColorGrayLight,
     colorWhite = ColorWhite,
